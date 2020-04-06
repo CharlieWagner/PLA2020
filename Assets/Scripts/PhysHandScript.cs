3faeb73517e2ea312a918a1714c50e2537c68383
Charlie Wagner <c.wagner@student.isartdigital.com>
2020-04-06 19:51:45 +0200

    Targeting & Object shooting

--- Assets/Scripts/PhysHandScript.cs
+++ Assets/Scripts/PhysHandScript.cs
@@ -4,30 +4,44 @@ using UnityEngine;
 
 public class PhysHandScript : MonoBehaviour
 {
+    
+    [SerializeField]
+    private HandScript _ParentHand;// Controller
+    [SerializeField]
+    private LayerMask GrabMask;    // Grabbing mask, excludes player
+    [SerializeField]
+    private int side = 1;          // the side of the hand
 
-    private FixedJoint _GrabJoint;
-    [SerializeField]
-    private HandScript _ParentHand;
-    [SerializeField]
-    private LayerMask GrabMask;
-    [SerializeField]
-    private int side = 1;
-
-    [SerializeField]
-    private Transform _ShootSpot;
-    [SerializeField]
-    private float _Range;
+    private FixedJoint _GrabJoint; // the fixed joint used for grabbing stuff
 
     [SerializeField]
-    private LayerMask _DynamicObjectLayerMask;
+    private Transform _ShootSpot;  // origin of the shooting
+    [SerializeField]
+    private float _Range;          // Shooting Range
+    [SerializeField]
+    private float _ObjectAimSpread;// Spread of the bject Auto-Aim
+    
+    private GameObject _CurrentTarget; // Current target Object
+    private Vector3 _AimPos;           // Current Target Position
 
     [SerializeField]
-    private LayerMask _NotDynamicObject;
+    private LayerMask _DynamicObjectLayerMask; // Mask for dynamic object targeting
+    [SerializeField]
+    private LayerMask _NotDynamicObject; // Mask for regular object targeting
 
     [SerializeField]
-    private Animator _HandAnimator;
+    private Animator _HandAnimator; // Hand mesh animator
     [SerializeField]
-    private Animator _ReticleAnimator;
+    private Animator _ReticleAnimator; // Reticle object animator
+    [SerializeField]
+    private LineRenderer _AimLine;      // Aiming line renderer
+
+    private int _RangeStatus;            // Range Status, 0 no range, 1 in range, 2 shooting
+    private bool _TargetIsObject;        // if target is object
+    private ConfigurableJoint _ObjectShootJoint; // Shooting Joint
+
+    [SerializeField]
+    private LineRenderer _ShootLine; // Shooting Line Renderer
 
     private bool _shooting;
 
@@ -38,31 +52,15 @@ public class PhysHandScript : MonoBehaviour
 
     private void Update()
     {
-        TargetStatus();
-        Grab();
-    }
+        if (_RangeStatus != 2)
+            TargetStatus();
 
-    private void TargetStatus()
-    {
-        RaycastHit _TargetInfo;
-        if (Physics.Raycast(_ShootSpot.position, _ShootSpot.forward, out _TargetInfo, _Range, _DynamicObjectLayerMask))
-        {
-            //Debug.Log("Dynamic");
-            _ReticleAnimator.SetInteger("RangeStatus", 1);
-            _ReticleAnimator.SetBool("isObject", true);
-        }
-        else if (Physics.Raycast(_ShootSpot.position, _ShootSpot.forward, out _TargetInfo, _Range, _NotDynamicObject))
-        {
-            //Debug.Log("Not Dynamic");
-            _ReticleAnimator.SetInteger("RangeStatus", 1);
-            _ReticleAnimator.SetBool("isObject", false);
-        }
-        else
-        {
-            //Debug.Log("nothing");
-            _ReticleAnimator.SetInteger("RangeStatus", 0);
-            _ReticleAnimator.SetBool("isObject", false);
-        }
+        LineUpdate();
+        ReticleUpdate();
+
+        Shoot();
+
+        Grab();
     }
 
     private void Grab()
@@ -96,4 +94,112 @@ public class PhysHandScript : MonoBehaviour
             Destroy(_GrabJoint);
         }
     }
+
+    private void TargetStatus()
+    {
+        RaycastHit _TargetInfo;
+        //if (Physics.Raycast(_ShootSpot.position, _ShootSpot.forward, out _TargetInfo, _Range, _DynamicObjectLayerMask))
+        if (Physics.SphereCast(_ShootSpot.position, _ObjectAimSpread, _ShootSpot.forward, out _TargetInfo, _Range, _DynamicObjectLayerMask))
+        {
+            _RangeStatus = 1;
+            _TargetIsObject = true;
+
+            _CurrentTarget = _TargetInfo.collider.gameObject;
+
+        }
+        else if (Physics.Raycast(_ShootSpot.position, _ShootSpot.forward, out _TargetInfo, _Range, _NotDynamicObject))
+        {
+            _RangeStatus = 1;
+            _TargetIsObject = false;
+
+            _CurrentTarget = null;
+        }
+        else
+        {
+            _RangeStatus = 0;
+            _TargetIsObject = false;
+
+            _CurrentTarget = null;
+        }
+        _AimPos = _TargetInfo.point;
+    }
+
+    private void LineUpdate()
+    {
+        if (_RangeStatus == 1)
+            _AimLine.SetPosition(1, _AimPos);
+        else
+            _AimLine.SetPosition(1, _AimLine.transform.position);
+
+        _AimLine.SetPosition(0, _AimLine.transform.position);
+    }
+
+    private void ReticleUpdate()
+    {
+        _ReticleAnimator.SetInteger("RangeStatus", _RangeStatus);
+        _ReticleAnimator.SetBool("isObject", _TargetIsObject);
+    }
+
+    private void Shoot()
+    {
+        if (_ParentHand._Shoot.GetStateDown(_ParentHand._handType) && _RangeStatus == 1) // Shoot press
+        {
+            switch(_TargetIsObject)
+            {
+                case true : //   Target is object
+
+                    _ObjectShootJoint = gameObject.AddComponent<ConfigurableJoint>();
+                    _ObjectShootJoint.connectedBody = _CurrentTarget.GetComponent<Rigidbody>();
+                    initObjectShootJoint(_ObjectShootJoint);
+
+                    _RangeStatus = 2;
+                    break;
+                case false : //  Target is not object
+                    _RangeStatus = 2;
+                    break;
+            }
+        }
+
+        if (_ParentHand._Shoot.GetStateUp(_ParentHand._handType) && _RangeStatus == 2) // Shoot release
+        {
+            switch (_TargetIsObject)
+            {
+                case true: //   Target is object
+                    
+                    Destroy(_ObjectShootJoint);
+                    _CurrentTarget = null;
+
+                    _RangeStatus = 0;
+                    break;
+                case false: //  Target is not object
+                    _RangeStatus = 0;
+                    break;
+            }
+        }
+    }
+
+
+    private void initObjectShootJoint(ConfigurableJoint Joint) // -------------------------------------------- Object Shoot joint initialiser
+    {
+        Joint.autoConfigureConnectedAnchor = false;
+        Joint.connectedAnchor = Vector3.zero;
+
+        Joint.secondaryAxis = Vector3.zero;
+        Joint.axis = Vector3.zero;
+        /*
+        limits LLS = new JointDrive();
+        jd.positionSpring = 200f;
+
+        Joint.linearLimitSpring = LLS;
+        */
+        JointDrive jd = new JointDrive();
+        jd.positionSpring = 50f;
+        jd.positionDamper = 1f;
+        jd.maximumForce = 50f;
+
+        Joint.xDrive = jd;
+        Joint.yDrive = jd;
+        Joint.zDrive = jd;
+    }
+
 }
